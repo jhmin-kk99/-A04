37824242d9185d54a1fbb56bdbec5cedf889b754
CHANGYU <window9u@konkuk.ac.kr>
2023-10-02 21:47:51 +0900

    utility - 함수 추가
    is_valid 함수 에러 리턴 ver 추가

--- utility.py
+++ utility.py
@@ -6,29 +6,52 @@ def is_valid_title(input_string):
     if re.match(pattern, input_string):
         return True
     else:
-        # if len(input_string) < 1 or len(input_string) > 10:
-        #     print("길이가 1 이상 10 이하가 아닙니다.")
-        # if not input_string[0].isalpha() and not input_string[0].isspace():
-        #     print("첫 문자가 알파벳 또는 띄어쓰기(␣)가 아닙니다.")
-        # if not re.match(r'[a-zA-Z가-힣\s]*$', input_string):
-        #     print("알파벳, 한글, 띄어쓰기(␣) 이외의 문자가 포함되어 있습니다.")
         return False
+
+def is_valid_title_str(input_string):
+    pattern = r'^(?![␣])[␣a-zA-Z가-힣]{1,10}$'
+    if re.match(pattern, input_string):
+        return "True"
+    else:
+        if len(input_string) < 1 or len(input_string) > 10:
+            return "오류: 길이가 1 이상 10 이하가 아닙니다."
+        if not input_string[0].isalpha() and not input_string[0].isspace():
+            return "오류: 첫 문자가 알파벳 또는 띄어쓰기(␣)가 아닙니다."
+        if not re.match(r'[a-zA-Z가-힣\s]*$', input_string):
+            return "오류: 알파벳, 한글, 띄어쓰기(␣) 이외의 문자가 포함되어 있습니다."
+        return "False"
+
 
 def is_valid_date(input_date):
     try:
-        # 문자열을 날짜로 파싱합니다.
         datetime.strptime(input_date, '%Y-%m-%d')
         return True
     except ValueError:
-        # 유효하지 않은 날짜 형식인 경우 예외가 발생합니다.
         return False
 
-def is_valid_deadline(input_string):
+def is_valid_date_str(input_date):
+    try:
+        datetime.strptime(input_date, '%Y-%m-%d')
+    except ValueError:
+        return "오류: 날짜를 입력해 주세요."
+
+    year, month, day = map(int, input_date.split('-'))
+
+    if year < 1000 or year > 9999 or month < 1 or month > 12 or day < 1 or day > 31:
+        return "오류: 날짜는 YYYY-MM-DD 형태로 입력해야 합니다."
+
+    if (month in [4, 6, 9, 11] and day > 30) or (month == 2 and day > 29):
+        return "오류: 정의되지 않은 날짜 입니다."
+    return "True"
+
+
+def is_valid_repeat(input_string):
     valid=["없음", "매주", "매달", "매년"]
     if(input_string in valid):
         return True
     else:
         return False
+
 
 def is_last_day_of_month(date):
     next_month = date.replace(day=28) + timedelta(days=4)
