01d1ea92373fc3438be51305d9375c2475eb680b
CHANGYU <window9u@konkuk.ac.kr>
2023-10-02 21:49:27 +0900

    FileManager - 함수명 변경

--- FileManager.py
+++ FileManager.py
@@ -18,8 +18,8 @@ class FileManager:
         self.df = self.df.sort_values(by='마감 날짜')
         self.df.to_csv(self.file_path, index=False)
 
-    def returnRowByIndex(self, index):
-        return self.df.iloc[index]
+    def getDataByIndex(self, index):
+        return self.df.loc[index].to_list()
 
     def filterTodoList(self):
         years_df = self.df[(self.df['반복'] == "매년") & self.df['마감 날짜'].apply(filter_by_year)]
@@ -35,7 +35,7 @@ class FileManager:
             row_data = {'index': index, 'data': row.to_dict()}
             if (is_valid_title(row_data['data']['작업 이름'])
                     and is_valid_date(row_data['data']['마감 날짜'])
-                    and is_valid_deadline(row_data['data']['반복'])):
+                    and is_valid_repeat(row_data['data']['반복'])):
                 continue
             else:
                 print(f'오류: 데이터 파일 TodoList_Datas.csv에 문법 규칙과 의미 규칙에 위배되는 행이 {index + 2}행에 존재합니다.')
@@ -46,11 +46,11 @@ class FileManager:
         self.df.drop(index, inplace=True)
         self.df.to_csv(self.file_path, index=False)
 
-    def editTodo(self, index, col_name,data):
+    def editTodo(self, index, col_name,data):##col name:'작업 이름'
         self.df.loc[index,col_name]=data
         self.df.to_csv(self.file_path, index=False)
 
-    def addTodo(self,data):
+    def addTodo(self,data):## data: [배열임]
         new_row=pd.DataFrame({'작업 이름':[data[0]],'마감 날짜':[data[1]],'반복':[data[2]]})
         self.df=pd.concat([self.df,new_row],ignore_index=True)##index 초기화
         self.df.to_csv(self.file_path, index=False)
