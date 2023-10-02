4f2d54e196adf43024505f982c2f528951b26441
CHANGYU <window9u@konkuk.ac.kr>
2023-10-02 12:45:46 +0900

    main()에 maininterface 구현

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
-from userInterFace import *
 from FileManager import FileManager
-
+from mainInterface import MainInterface
 def main():
     file_manager=FileManager()
+    main_interface = MainInterface()
     if(not file_manager.isValidFile()):
         return
     file_manager.sortTodoList()
-    print(file_manager.filterTodoList())
-    # while(True):
-    #     menu=mainInterface()
-    #     if(menu==1):
-    #         todoListInterface()
-    #     elif(menu==2):
-    #         print(2)
-    #     else:
-    #         print("프로그램을 종료합니다.")
-    #         exit()
-
-
+    while True:
+        menu=main_interface.CLI()
+        if(menu==1):
+            print(1)
+        elif (menu==2):
+            print(2)
+        else:
+            break
 
 main()
