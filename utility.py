d524966e4c6b8192de84d360e2b266bbc0e6fd06
CHANGYU <window9u@konkuk.ac.kr>
2023-10-06 12:34:37 +0900

    feat: change_date_to_week
    옛날 날짜를 현재 날짜 기준으로 mapping 하는 함수 추가

--- utility.py
+++ utility.py
@@ -1,5 +1,6 @@
 import re
-from datetime import datetime, date,timedelta
+from datetime import datetime, date, timedelta
+
 
 def is_valid_title(input_string):
     pattern = r'^(?![␣])[␣a-zA-Z가-힣1-9]{1,10}$'
@@ -7,6 +8,7 @@ def is_valid_title(input_string):
         return True
     else:
         return False
+
 
 def is_valid_title_str(input_string):
     pattern = r'^(?![␣])[␣a-zA-Z가-힣1-9]{1,10}$'
@@ -29,6 +31,7 @@ def is_valid_date(input_date):
     except ValueError:
         return False
 
+
 def is_valid_date_str(input_date):
     try:
         datetime.strptime(input_date, '%Y-%m-%d')
@@ -46,52 +49,109 @@ def is_valid_date_str(input_date):
 
 
 def is_valid_repeat(input_string):
-    valid=["없음", "매주", "매달", "매년"]
-    if(input_string in valid):
+    valid = ["없음", "매주", "매달", "매년"]
+    if (input_string in valid):
         return True
     else:
         return False
 
 
-def get_valid_date(year,month,day):
-    if(is_valid_date(str(year)+"-"+str(month)+"-"+str(day))):
-        return datetime(year,month,day).date()
+def get_valid_date(year, month, day):
+    if (is_valid_date(str(year) + "-" + str(month) + "-" + str(day))):
+        return datetime(year, month, day).date()
     else:
-        if(month==13):
-            return get_valid_date(year+1,1,day)
-        return get_valid_date(year,month,day-1)
+        if (month == 13):
+            return get_valid_date(year + 1, 1, day)
+        return get_valid_date(year, month, day - 1)
+
+
+def change_date_to_this_week_year(input_date):
+    today = date.today()
+    input = datetime.strptime(input_date, "%Y-%m-%d").date()
+    return get_valid_date(today.year, input.month, input.day).strftime("%Y-%m-%d")
+
+
+def change_date_to_this_week_month(input_date):
+    today = date.today()
+    input = datetime.strptime(input_date, "%Y-%m-%d").date()
+    this_month_date = get_valid_date(today.year, today.month, input.day)
+    next_month_date = get_valid_date(today.year, today.month + 1, input.day)
+    if (is_in_7days(this_month_date)):
+        return this_month_date.strftime("%Y-%m-%d")
+    elif (is_in_7days(next_month_date)):
+        return next_month_date.strftime("%Y-%m-%d")
+    else:
+        return "1111-11-11"
+
+
+def change_date_to_this_week_weekday(input_date):
+    today = date.today()
+    input = datetime.strptime(input_date, "%Y-%m-%d").date()
+    while (today.weekday() != input.weekday()):
+        today = today + timedelta(days=1)
+    return (today).strftime("%Y-%m-%d")
+
+
+
+def change_date_to_this_week(input_date, repeat):
+    today = date.today()
+    input = datetime.strptime(input_date, "%Y-%m-%d").date()
+    if (repeat == "없음"):
+        return input_date
+    elif (repeat == "매주"):
+        while (today.weekday() != input.weekday()):
+            today = today + timedelta(days=1)
+        else:
+            return (today).strftime("%Y-%m-%d")
+    elif (repeat == "매달"):
+        this_month_date = get_valid_date(today.year, today.month, input.day)
+        next_month_date = get_valid_date(today.year, today.month + 1, input.day)
+        if (is_in_7days(this_month_date)):
+            return this_month_date.strftime("%Y-%m-%d")
+        elif (is_in_7days(next_month_date)):
+            return next_month_date.strftime("%Y-%m-%d")
+        else:
+            return "1111-11-11"
+    elif (repeat == "매년"):
+        return get_valid_date(today.year, input.month, input.day).strftime("%Y-%m-%d")
+    else:
+        return "1111-11-12"  ##오류
+
 
 def is_in_7days(input_date):
-    today=date.today()
-    return (input_date-today).days<=7 and (input_date-today).days>=0
+    today = date.today()
+    return (input_date - today).days <= 7 and (input_date - today).days >= 0
+
 
 def is_later_todo(input_date):
-    today=date.today()
-    return (input_date-today).days>7
+    today = date.today()
+    return (input_date - today).days > 7
 
 
 def filter_by_year(input_date):
-    today=date.today()
-    input=datetime.strptime(input_date, "%Y-%m-%d").date()
+    today = date.today()
+    input = datetime.strptime(input_date, "%Y-%m-%d").date()
     if (is_later_todo(input)):
         return False
-    return is_in_7days(get_valid_date(today.year, input.month, input.day))##말일처리 다시
+    return is_in_7days(get_valid_date(today.year, input.month, input.day))  ##말일처리 다시
 
 
 def filter_by_month(input_date):
-    today=date.today()
-    input=datetime.strptime(input_date, "%Y-%m-%d").date()
-    if(is_later_todo(input)):##마감일이 오늘보다 늦으면
+    today = date.today()
+    input = datetime.strptime(input_date, "%Y-%m-%d").date()
+    if (is_later_todo(input)):  ##마감일이 오늘보다 늦으면
         return False
-    this_month_date=get_valid_date(today.year, today.month, input.day)
-    next_month_date=get_valid_date(today.year, today.month+1, input.day)
+    this_month_date = get_valid_date(today.year, today.month, input.day)
+    next_month_date = get_valid_date(today.year, today.month + 1, input.day)
     return is_in_7days(this_month_date) or is_in_7days(next_month_date)
+
 
 def filter_by_week(input_date):
     input = datetime.strptime(input_date, "%Y-%m-%d").date()
     if (is_later_todo(input)):
         return False
     return True
+
 
 def filter_by_none(input_date):
     input = datetime.strptime(input_date, "%Y-%m-%d").date()
